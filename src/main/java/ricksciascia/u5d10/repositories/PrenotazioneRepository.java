c28fff32fcd63840f14e3e937eb8f40313815933
Riccardo <richi-97@live.it>
2026-02-13 16:25:55 +0100

    POST prenotazione con query controllo se dipendentecontrollare che il dipendente non abbia già un viaggio programmato per quel giorno

--- src/main/java/ricksciascia/u5d10/repositories/PrenotazioneRepository.java
+++ src/main/java/ricksciascia/u5d10/repositories/PrenotazioneRepository.java
@@ -2,9 +2,13 @@ package ricksciascia.u5d10.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ricksciascia.u5d10.entities.Dipendente;
 import ricksciascia.u5d10.entities.Prenotazione;
+
+import java.time.LocalDate;
 
 @Repository
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione,Long> {
 //    TODO: eventuali queries
+    boolean existsByDipendenteAndViaggioData(Dipendente dipendente, LocalDate data);
 }
